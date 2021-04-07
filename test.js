b60c76fc7e4b3581e40eab9ab7f25d33aae35bce
thanhdo <190041vu@yse-c.net>
2021-04-07 20:11:47 +0900

    update 3

--- test.js
+++ test.js
@@ -6,3 +6,11 @@ var server = http.createServer(
     }
 );
 server.listen(3000);
+const http = require('http');
+
+var server = http.createServer(
+    (request, Response)=>{
+        Response.end('hello Node');
+    }
+);
+server.listen(3000);
