422b7b98c82f42086fc8886cc3d3d243dff0cbc5
thanhdo <190041vu@yse-c.net>
2021-04-07 20:03:08 +0900

    訂正した

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const http = require('http');
 var server = http.createServer(
     (request, Response)=>{
         Response.end('hello Node');
+        Response.end ('こんにちは！');
     }
 );
 server.listen(3000);
