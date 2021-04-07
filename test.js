1e6511b90e98e185cca484c77a1c08dd4f0927a5
nguyenthanhdo91 <71994566+nguyenthanhdo91@users.noreply.github.com>
2021-04-07 19:58:11 +0900

    Update test.js
    xoa du lieu

--- test.js
+++ test.js
@@ -3,7 +3,6 @@ const http = require('http');
 var server = http.createServer(
     (request, Response)=>{
         Response.end('hello Node');
-        Response.end ('こんにちは！')
     }
 );
 server.listen(3000);
